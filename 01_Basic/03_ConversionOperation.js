21b72016735a0b4194cfdae0c80ba7e918124891
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-02 13:20:25 +0000

    Conversion of DataType

---
+++ 01_Basic/03_ConversionOperation.js
@@ -0,0 +1,14 @@
+let score="true"
+
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
