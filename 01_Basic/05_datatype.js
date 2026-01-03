8acb87510b56c0e41ca1a46e8886894a296e70b2
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-03 07:03:09 +0000

    Datatype

---
+++ 01_Basic/05_datatype.js
@@ -0,0 +1,33 @@
+// Primitive Datatype
+
+//7types of Primitive Datatype
+//: Sting, Number, Bollearn, Null, Undefined, Symbol, BigInt
+
+const outsideTemp = null
+let userEmail;
+
+const id=Symbol('123')
+const anotherid=Symbol('123')
+
+console.log(id==anotherid);
+
+//const bigNumber = 32546574321n
+
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman"/ "naagaraj","doga"];
+let myObj={
+    name: "Sonu",
+    age: 22,
+
+}
+const myFunction=function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof anotherid);
