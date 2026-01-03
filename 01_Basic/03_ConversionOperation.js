eadf3ef4605b2a4a4fa500b7655cd003f97eed23
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-03 06:16:26 +0000

    04_Comparision

--- 01_Basic/03_ConversionOperation.js
+++ 01_Basic/03_ConversionOperation.js
@@ -12,3 +12,32 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+let str1="Sonu"
+let str2=" Pandit"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1" +2 +2);
+console.log(1 +2 + "2");
+
+console.log(true);
+
+num1 = num2 = num3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
