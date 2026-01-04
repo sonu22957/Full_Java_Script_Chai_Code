9cfcf30413c3737d867e404565e4d153fa9bb823
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-04 16:18:35 +0000

    05String

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,45 @@
+const name ="Sonu"
+const repoCount = 50
+
+//console.log(name + repoCount + "Value");
+
+console.log('Helo my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('Son-u')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('o'));
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne=" Sonu "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(newStringOne.trim());
+
+const url = "https://sonupanditbth%20pandit"
+
+console.log(url.replace('20%','-'));
+
+console.log(url.includes('sundar'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_Basic/Interview.js
@@ -0,0 +1,3 @@
+console.log(Object.is(+0,-0))
+console.log([1,,3].length);
+console.log(!!(0||""||5));
