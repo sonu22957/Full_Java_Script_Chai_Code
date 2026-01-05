da476049773d0079b8ab7206630b31861dfa0f57
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-05 15:51:10 +0000

    Interview Js

--- 01_Basic/Interview.js
+++ 01_Basic/Interview.js
@@ -1,3 +1,12 @@
 console.log(Object.is(+0,-0))
 console.log([1,,3].length);
 console.log(!!(0||""||5));
+console.log(Math.trunc(-3.7));
+console.log("5"**2);
+console.log(false||true);
+console.log(0 ?? false ?? "js");
+
+
+
+
+
