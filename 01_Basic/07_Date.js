51c05ff3ad58df7335be12a6a0a81bbb917796e7
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-08 02:53:45 +0000

    07_Date

---
+++ 01_Basic/07_Date.js
@@ -0,0 +1,34 @@
+//Date
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+
+//let myCreateDate = new Date(2026,0,8)
+//console.log(myCreateDate.toDateString());
+//console.log(myCreateDate.toLocaleStringSDateString());
+//console.log(myCreateDate.toLocaleDateString());
+
+let myCreateDate = new Date("04-15-2025")
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime);
+console.log(Date.now);
+
+newDate.toLocaleDateString('default', {
+    weekday: "long"
+})
+
+
+
+
+
+
+
