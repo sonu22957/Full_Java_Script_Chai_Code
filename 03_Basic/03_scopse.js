a54097006085902670146afc35637aa18a8dbc42
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-13 05:14:16 +0000

    03_Scope

---
+++ 03_Basic/03_scopse.js
@@ -0,0 +1,58 @@
+
+// var c=300
+// let a=300
+// if (true) {
+// let a=10
+// const b=20
+// var c=30
+// console.log("You:",a);
+
+
+// }
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+function one(){
+    const username = "Sonu"
+
+    function two(){
+        const website = "youtube"
+    //console.log(username);
+    
+}
+//console.log(website);
+
+two()
+}
+
+//one()
+
+if (true) {
+    const username = "Sonu"
+    if (username == "Sonu") {
+        const website = "youtube"
+        //console.log(username + website);
+        
+        
+    }
+   
+    
+}
+
+//++++++++++++++++++++++++++++ intersting ++++++++
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
