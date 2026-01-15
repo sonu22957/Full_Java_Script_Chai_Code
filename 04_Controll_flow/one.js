229a17583164b2206b40a63612236d036f0db718
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-15 08:53:15 +0000

    one_If_els

---
+++ 04_Controll_flow/one.js
@@ -0,0 +1,66 @@
+//if
+
+// const isUserloggedIn = true
+// const temperature =41
+
+// if (temperature === 40) {
+//     console.log("less than 50");
+    
+    
+// }else{
+// console.log("temperature is greater than 50");
+// }
+//<, >, <=, >=, ==, !=, ===
+
+
+// const score =200
+
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`User powe: ${power}`);
+    
+    
+// }
+// console.log(`User powe: ${power}`);
+
+
+//const balance = 1000
+
+// if (balance > 500) console.log("test"), //implicite scop
+// console.log("test2");
+
+
+    // if (balance < 500) {
+    //     console.log("less than");
+        
+
+        
+    // }else if (balance < 750){
+    //     console.log("less than 750");
+        
+    // }else if (balance < 900){
+    //     console.log("less than 750");
+
+    // }else{
+    //     console.log("less than 1200");
+        
+    // }
+
+
+
+    const isUserloggedIn = true
+    const debitCard=true
+    const loggedInFromGoogle=false
+    const loggedInFromEmail = true
+
+    if (isUserloggedIn && debitCard && 2==2) {
+        console.log("Allow to buy course");
+        
+        
+    }
+
+    if (loggedInFromGoogle || loggedInFromEmail ) {
+        console.log("User logge in");
+        
+        
+    }
