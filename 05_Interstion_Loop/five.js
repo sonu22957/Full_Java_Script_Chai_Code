bd5cde6705068e5bb7af9e7ff1403695552e9878
Sonu Pandit <sonupanditbth2003@gmail.com>
2026-01-17 18:18:07 +0000

    five

---
+++ 05_Interstion_Loop/five.js
@@ -0,0 +1,47 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+    
+//})
+
+//erro function
+// coding.forEach( (item) =>{
+//     console.log(item);
+    
+// })
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=>{
+//     console.log(item,index, arr);
+    
+// })
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageName: "js"
+    },
+    {
+        languageName: "java",
+        languageName: "java"
+    },
+    {
+        languageName: "python",
+        languageName: "python"
+    }   
+    
+    
+
+]
+myCoding.forEach( (item) =>{
+
+    console.log(item.languageName);
+    
+
+})
